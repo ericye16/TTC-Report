8ef156fa030d9420910a6d1a2b5270fbe3f56b97
Eric Ye <ericye16@gmail.com>
2014-04-25 10:55:44 -0400

    routes

--- getAgencies.py
+++ getAgencies.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 import xml.etree.ElementTree as ET
-##from urllib import request
+from urllib import request
+AGENCY = 'ttc'
 ##agencyList = request.urlopen(
 ##    'http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList').read()
 agencyList = open('agencyList').read()
-tree = ET.fromstring(agencyList)
-for agency in tree:
-    if agency.attrib['tag'] == 'ttc':
+agencyTree = ET.fromstring(agencyList)
+for agency in agencyTree:
+    if agency.attrib['tag'] == AGENCY:
         for k, v in agency.attrib.items():
             print(k, v)
+
+routeList = request.urlopen(
+    'http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=' + AGENCY).read()
+
+routeListTree = ET.fromstring(routeList)
