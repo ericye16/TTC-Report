f55f69aea77526865994fb44e4fe8fca62405ecf
Eric Ye <ericye16@gmail.com>
2014-04-26 19:24:22 -0400

    more geographic things with titles

--- parseSchedules.py
+++ parseSchedules.py
@@ -24,7 +24,15 @@ class Trip:
                                    self.trip_headsign)
 
 class Stop:
-    pass
+    def __init__(self, stop_id, stop_name, stop_lat, stop_lon):
+        self.stop_id = stop_id
+        self.stop_name = stop_name
+        self.stop_lat = stop_lat
+        self.stop_lon = stop_lon
+
+    def __repr__(self):
+        return "Stop(%s,%s,%s,%s)" % (self.stop_id,self.stop_name,self.stop_lat,
+                                         self.stop_lon)
 
 
 def show_routes_all(routes_list):
@@ -34,15 +42,16 @@ def show_routes_all(routes_list):
             print("\t%s %s" %(k, trip))
 if __name__ == '__main__':
     routes_list = {}
+    stops_list = {}
 
     with open('schedules/routes.txt') as routes_file: 
         routes_csv = csv.reader(routes_file)
         next(routes_csv) #skip header
         for route in routes_csv:
-            route_id = route[0]
+            route_id =int(route[0])
             route_short_name = route[2]
             route_long_name = route[3]
-            route_type = route[5]
+            route_type = int(route[5])
             routes_list[route_id] = Route(route_id, route_short_name,
                                           route_long_name, route_type)
 
@@ -50,17 +59,29 @@ if __name__ == '__main__':
         trips_csv = csv.reader(trips_file)
         next(trips_file)
         for trip in trips_csv:
-            route_id = trip[0]
-            service_id = trip[1]
-            trip_id = trip[2]
+            route_id = int(trip[0])
+            service_id =int(trip[1])
+            trip_id = int(trip[2])
             trip_headsign = trip[3]
             route = routes_list[route_id]
             route.trips[trip_id] = Trip(trip_id, service_id, trip_headsign)
 
-##    show_routes_all(routes_list)
+    with open('schedules/stops.txt') as stops_file:
+        stops_csv = csv.reader(stops_file)
+        next(stops_file)
+        for stop in stops_csv:
+            stop_id = int(stop[0])
+            stop_name = stop[2]
+            stop_lat = float(stop[4])
+            stop_lon = float(stop[5])
+            stops_list[stop_id] = Stop(stop_id, stop_name, stop_lat, stop_lon)
+
+    ## Distribution of trips vs routes
     trips_freq = []
     for route in routes_list.values():
         trips_freq.append((route.route_short_name, len(route.trips)))
+
+    #plt.figure()
     fig, ax = plt.subplots(1,1)
     n, bins, patches = ax.hist([x[1] for x in trips_freq], bins=25,
                                 align='mid', range=(0, 5000))
@@ -71,5 +92,18 @@ if __name__ == '__main__':
     ax.set_xticks(bins[:-1])
     plt.subplots_adjust(bottom=0.2)
     plt.savefig('figures/trip_routes_dist.png')
-    #plt.show()
+
+    ## Geographic distribution of bus stops
+    plt.figure(figsize=(8,6))
+    #plt.subplots(1,1)
+    lats = [x.stop_lat for x in stops_list.values()]
+    longs = [x.stop_lon for x in stops_list.values()]
+    plt.scatter(longs, lats, marker='.')
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title('Geographic Distribution of All Stops')
+    plt.savefig('figures/geographic_stop_dist.png', dpi=300)
+
+
+    plt.show()
 
