2d24ac37f42135e91dd5ed1e1b6a356cb89a8792
Eric Ye <ericye16@gmail.com>
2014-04-26 16:55:00 -0400

    more schedule parsing logic-- with trips

--- parseSchedules.py
+++ parseSchedules.py
@@ -5,24 +5,54 @@ class Route:
         self.route_short_name = route_short_name
         self.route_long_name = route_long_name
         self.route_type = route_type
+        self.trips = {}
 
     def __repr__(self):
         return "Route(%s,%s,%s,%s)" % (self.route_id, self.route_short_name,
                                        self.route_long_name, self.route_type)
-        
+class Trip:
+    def __init__(self, trip_id, trip_headsign):
+        self.trip_id = trip_id
+        self.trip_headsign = trip_headsign
+        self.stops = {}
+
+    def __repr__(self):
+        return "Trip(%s,%s)" % (self.trip_id, self.trip_headsign)
+
 class Stop:
     pass
 
-routes_list = []
 
-with open('schedules/routes.txt') as routes_file: 
-    routes_csv = csv.reader(routes_file)
-    next(routes_csv) #skip header
-    for route in routes_csv:
-        route_id = route[0]
-        route_short_name = route[2]
-        route_long_name = route[3]
-        route_type = route[5]
-        routes_list.append(Route(route_id, route_short_name, route_long_name,
-                                 route_type))
-        
+def show_routes_all(routes_list):
+    for k, route in sorted(routes_list.items()):
+        print(k, route)
+        for k, trip in sorted(route.trips.items()):
+            print("\t%s %s" %(k, trip))
+if __name__ == '__main__':
+    routes_list = {}
+
+    with open('schedules/routes.txt') as routes_file: 
+        routes_csv = csv.reader(routes_file)
+        next(routes_csv) #skip header
+        for route in routes_csv:
+            route_id = route[0]
+            route_short_name = route[2]
+            route_long_name = route[3]
+            route_type = route[5]
+            routes_list[route_id] = Route(route_id, route_short_name,
+                                          route_long_name, route_type)
+
+    with open('schedules/trips.txt') as trips_file:
+        trips_csv = csv.reader(trips_file)
+        next(trips_file)
+        for trip in trips_csv:
+            route_id = trip[0]
+            trip_id = trip[2]
+            trip_headsign = trip[3]
+            route = routes_list[route_id]
+            route.trips[trip_id] = Trip(trip_id, trip_headsign)
+
+##    show_routes_all(routes_list)
+    trips_freq = []
+    for route in routes_list.values():
+        trips_freq.append((route.route_short_name, len(route.trips)))
