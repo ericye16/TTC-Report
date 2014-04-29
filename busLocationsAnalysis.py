e33d0340da206c470ec928c3ee85038393385636
Eric Ye <ericye16@gmail.com>
2014-04-29 11:52:03 -0400

    Wow. Very sqlite3

--- busLocationsAnalysis.py
+++ busLocationsAnalysis.py
@@ -1,27 +1,29 @@
-import pickle, glob, sys
+import pickle, glob, sys, sqlite3
 from busLocations import Vehicle
 
 if __name__ == '__main__':
+    db = sqlite3.connect("busLocations.sqlite3")
+    cur = db.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS vehicles(vId INTEGER, routeTag TEXT, dirTag TEXT, lat REAL,"
+                "lon REAL, heading INTEGER, timestamp INTEGER, UNIQUE(vId, timestamp))")
     dataFiles = glob.glob('vehicles-data/vehicles-*')
-    vehicles_dict = {}
     for i, dataFile in enumerate(sorted(dataFiles)):
         print("Processing file: %s with %.2f%% complete" % (dataFile, i * 100 / len(dataFiles)), file=sys.stderr)
         f = open(dataFile, 'rb')
         vehicles = pickle.load(f)
-        for vehicle in vehicles.values():
-            vId = vehicle.id
-            routeTag = vehicle.routeTag
-            dirTag = vehicle.dirTag
-            lat = vehicle.lat
-            lon = vehicle.lon
-            timestamp = vehicle.timestamp
-            if vId in vehicles_dict:
-                if abs(vehicles_dict[vId][-1].timestamp - timestamp) < 15e3:
-                    continue
-                else:
-                    vehicles_dict[vId].append(vehicle)
-            else:
-                vehicles_dict[vId] = [vehicle]
+        def vehicleGenerator(vehicles):
+            for vehicle in vehicles:
+                vId = int(vehicle.id)
+                routeTag = vehicle.routeTag
+                dirTag = vehicle.dirTag
+                lat = float(vehicle.lat)
+                lon = float(vehicle.lon)
+                timestamp = int(vehicle.timestamp)
+                heading = int(vehicle.heading)
+                yield (vId, routeTag, dirTag, lat, lon, heading, timestamp)
+        cur.executemany("INSERT OR IGNORE INTO vehicles VALUES(?,?,?,?,?,?,?)", vehicleGenerator(vehicles.values()))
+        if (i % 10 == 0):
+            db.commit()
         f.close()
-    with open("busLocationsAnalysis.pickle", "wb") as pickleFile:
-        pickle.dump(vehicles_dict, pickleFile)
+    db.commit()
+    db.close()
