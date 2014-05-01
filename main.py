ca65d3b7f2afdd6417d34d7ac08928828fc54bc0
Eric Ye <ericye16@gmail.com>
2014-05-01 19:06:30 -0400

    more in the notebook

--- main.py
+++ main.py
@@ -52,75 +52,76 @@ def pearsons_index(series):
     return 3 * (series.mean() - series.median()) / series.std()
 
 
-def main():
-    routes_list = {}
-    stops_list = {}
+#def main():
+routes_list = {}
+stops_list = {}
 
-    with open('schedules/routes.txt') as routes_file: 
-        routes_csv = csv.reader(routes_file)
-        next(routes_csv)  # skip header
-        for route in routes_csv:
-            route_id =int(route[0])
-            route_short_name = route[2]
-            route_long_name = route[3]
-            route_type = int(route[5])
-            routes_list[route_id] = Route(route_id, route_short_name,
-                                          route_long_name, route_type)
+with open('schedules/routes.txt') as routes_file: 
+    routes_csv = csv.reader(routes_file)
+    next(routes_csv)  # skip header
+    for route in routes_csv:
+        route_id =int(route[0])
+        route_short_name = route[2]
+        route_long_name = route[3]
+        route_type = int(route[5])
+        routes_list[route_id] = Route(route_id, route_short_name,
+                                      route_long_name, route_type)
 
-    with open('schedules/trips.txt') as trips_file:
-        trips_csv = csv.reader(trips_file)
-        next(trips_file)
-        for trip in trips_csv:
-            route_id = int(trip[0])
-            service_id =int(trip[1])
-            trip_id = int(trip[2])
-            trip_headsign = trip[3]
-            route = routes_list[route_id]
-            route.trips[trip_id] = Trip(trip_id, service_id, trip_headsign)
+with open('schedules/trips.txt') as trips_file:
+    trips_csv = csv.reader(trips_file)
+    next(trips_file)
+    for trip in trips_csv:
+        route_id = int(trip[0])
+        service_id =int(trip[1])
+        trip_id = int(trip[2])
+        trip_headsign = trip[3]
+        route = routes_list[route_id]
+        route.trips[trip_id] = Trip(trip_id, service_id, trip_headsign)
 
-    with open('schedules/stops.txt') as stops_file:
-        stops_csv = csv.reader(stops_file)
-        next(stops_file)
-        for stop in stops_csv:
-            stop_id = int(stop[0])
-            stop_name = stop[2]
-            stop_lat = float(stop[4])
-            stop_lon = float(stop[5])
-            stops_list[stop_id] = Stop(stop_id, stop_name, stop_lat, stop_lon)
+with open('schedules/stops.txt') as stops_file:
+    stops_csv = csv.reader(stops_file)
+    next(stops_file)
+    for stop in stops_csv:
+        stop_id = int(stop[0])
+        stop_name = stop[2]
+        stop_lat = float(stop[4])
+        stop_lon = float(stop[5])
+        stops_list[stop_id] = Stop(stop_id, stop_name, stop_lat, stop_lon)
 
-    ## Distribution of trips vs routes
-    trips_freq = []
-    for route in routes_list.values():
-        trips_freq.append((route.route_short_name, len(route.trips)))
-    trips_series = pd.Series([x[1] for x in trips_freq])
-    print(trips_series.describe())
-    print("Pearson's: %s" % pearsons_index(trips_series))
+## Distribution of trips vs routes
+trips_freq = []
+for route in routes_list.values():
+    trips_freq.append((route.route_short_name, len(route.trips)))
+trips_series = pd.Series([x[1] for x in trips_freq])
+print(trips_series.describe())
+print("Pearson's: %s" % pearsons_index(trips_series))
 
-    #plt.figure()
-    fig, ax = plt.subplots(1,1)
-    n, bins, patches = ax.hist([x[1] for x in trips_freq], bins=25,
-                                align='mid', range=(0, 5000))
-    plt.xlabel('Number of Trips')
-    plt.ylabel('Number of Routes')
-    plt.title('Distribution of trips over routes')
-    plt.xticks(rotation=70)
-    ax.set_xticks(bins[:-1])
-    plt.subplots_adjust(bottom=0.2)
-    plt.savefig('figures/trip_routes_dist.png')
+#plt.figure()
+fig, ax = plt.subplots(1,1)
+n, bins, patches = ax.hist([x[1] for x in trips_freq], bins=25,
+                            align='mid', range=(0, 5000))
+plt.xlabel('Number of Trips')
+plt.ylabel('Number of Routes')
+plt.title('Distribution of trips over routes')
+plt.xticks(rotation=70)
+ax.set_xticks(bins[:-1])
+plt.subplots_adjust(bottom=0.2)
+plt.savefig('figures/trip_routes_dist.png')
 
-    ## Geographic distribution of bus stops
-    plt.figure(figsize=(8,6))
-    #plt.subplots(1,1)
-    lats = [x.stop_lat for x in stops_list.values()]
-    longs = [x.stop_lon for x in stops_list.values()]
+## Geographic distribution of bus stops
+plt.figure(figsize=(8,6))
+#plt.subplots(1,1)
+lats = [x.stop_lat for x in stops_list.values()]
+longs = [x.stop_lon for x in stops_list.values()]
 
-    plt.scatter(longs, lats, marker='.', c='b')
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.title('Geographic Distribution of All Stops')
-    plt.savefig('figures/geographic_stop_dist.png', dpi=300)
+plt.scatter(longs, lats, marker='.', c='b')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Geographic Distribution of All Stops')
+plt.savefig('figures/geographic_stop_dist.png', dpi=300)
 
-    plt.show()
+plt.show()
 
-if __name__ == '__main__':
-    main()
+##if __name__ == '__main__':
+##    main()
+c
