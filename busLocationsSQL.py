a77a2112620f7e8338058e16c1a469a31f78ea44
Eric Ye <ericye16@gmail.com>
2014-05-05 02:23:08 -0400

    initial data

--- busLocationsSQL.py
+++ busLocationsSQL.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     cur.execute("CREATE TABLE IF NOT EXISTS vehicles(vId INTEGER, routeTag TEXT, dirTag TEXT, lat REAL,"
                 "lon REAL, heading INTEGER, timestamp INTEGER, UNIQUE(vId, timestamp))")
     dataFiles = glob.glob('vehicles-data/vehicles-*')
+    time_dist = []
     for i, dataFile in enumerate(sorted(dataFiles)):
         print("Processing file: %s with %.2f%% complete" % (dataFile, i * 100 / len(dataFiles)), file=sys.stderr)
         f = open(dataFile, 'rb')
@@ -22,8 +23,13 @@ if __name__ == '__main__':
                 heading = int(vehicle.heading)
                 yield (vId, routeTag, dirTag, lat, lon, heading, timestamp)
         cur.executemany("INSERT OR IGNORE INTO vehicles VALUES(?,?,?,?,?,?,?)", vehicleGenerator(vehicles.values()))
+        num_vehicles = len(vehicles)
+        time_stamp = int(dataFile.split('-')[2])
+        time_dist.append((time_stamp, num_vehicles))
         if (i % 10 == 0):
             db.commit()
         f.close()
     db.commit()
     db.close()
+    with open('time_dist.pickle', 'wb') as time_dist_pickle:
+        pickle.dump(time_dist, time_dist_pickle)
