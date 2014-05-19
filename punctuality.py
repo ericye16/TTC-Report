907ae2506d39936e0d270878474c6dfc69bb2509
Eric Ye <ericye16@gmail.com>
2014-05-19 19:32:23 -0400

    began punctuality...

--- punctuality.py
+++ punctuality.py
@@ -5,3 +5,11 @@ if __name__ == '__main__':
     client = MongoClient()
     db = client.datasummative
     vehicles = db.vehicles
+    stops = db.stops
+    stop_times = db.stop_times
+
+    # first pass: go through each stop and check if any of the correct buses are within 10m
+    # 10m corresponds to 4.5e-5 in degrees latitude and 6.2e-5 in degrees longitude
+    for stop in stops.find(limit=100):
+        pass
+        # TODO: finish
